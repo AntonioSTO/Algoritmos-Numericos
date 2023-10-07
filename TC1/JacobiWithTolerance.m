bba069192c83d962b8f9de53a56d2f42a86ce046
Antônio Sant'Ana de Oliveira <116594916+AntonioSTO@users.noreply.github.com>
2023-10-07 15:01:54 -0300

    Add files via upload

---
+++ TC1/JacobiWithTolerance.m
@@ -0,0 +1,21 @@
+function [x, k] = JacobiWithTolerance(A, b, tol)
+    n = size(A, 1);
+    x = zeros(n, 1); % Vetor de partida
+    x_old = x; % Inicializar com o vetor de partida
+    k = 1;
+
+    while k<=100000
+        x_old = x; % Salvar a iteração anterior
+        for i = 1:n
+            sum1 = A(i, 1:i-1) * x_old(1:i-1);
+            sum2 = A(i, i+1:n) * x_old(i+1:n);
+            x(i) = (b(i) - sum1 - sum2) / A(i, i);
+        end
+        k = k + 1;
+        if norm(x - x_old) < tol
+            break; % Convergência atingida
+        end
+    end
+end
+
+
